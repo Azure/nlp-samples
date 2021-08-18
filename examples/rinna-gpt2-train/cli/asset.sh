9bfb21df9469dbdb262faa8608da60736d1ce72f
Keita Onabuta <keonabut@microsoft.com>
2021-08-18 07:00:58 +0000

    change yml path

--- examples/rinna-gpt2-train/cli/asset.sh
+++ examples/rinna-gpt2-train/cli/asset.sh
@@ -4,5 +4,5 @@
 az ml compute create -n gpu-cluster --type AmlCompute  --min-instances 0 --max-instances 4 --size Standard_NC8as_T4_v3
 
 # environment
-az ml environment create --file huggingface_ort.yml
+az ml environment create --file huggingface_ort_env.yml
 
