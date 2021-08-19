0b7401647fe8812f3c68f1257d441f7cf287fd70
konabuta <keonabut@microsoft.com>
2021-08-19 01:21:08 +0000

    delete azureml workspace creation

--- examples/rinna-gpt2-train/cli/setup.sh
+++ examples/rinna-gpt2-train/cli/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 az extension add -n ml -y
-az group create -n "azureml" -l "japaneast"
-az configure --defaults group="azureml" workspace="azureml"
-az ml workspace create
+az configure --defaults group="azureml-mlops" workspace="azureml-mlops"
