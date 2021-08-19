36158f33f79870d5a94a91878d0aca8aa6dc9f86
ShuntaIto <shunta.ito@microsoft.com>
2021-08-19 09:58:28 +0000

    Deploy model

--- examples/rinna-gpt2-predict/src/score.py
+++ examples/rinna-gpt2-predict/src/score.py
@@ -1,15 +1,18 @@
-import mlflow.pyfunc
+import sys
+import json
+import os
+import onnxruntime
+import time
 
-class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
+import torch
+import numpy
+from transformers import T5Tokenizer
+from onnxruntime.transformers.gpt2_beamsearch_helper import Gpt2BeamSearchHelper, GPT2LMHeadModel_BeamSearchStep
+
+class ONNXrinnaGPT2:
     def __init__(self, gpt2_onnx_path, num_tokens_to_produce = 30, beam_size=4, use_onnxruntime_io=False, use_cpu=True):
-        import os
-        import torch
         self.cache_dir = os.path.join(".", "cache_models")
-        self.onnx_bytes = self.get_onnx_bytes(gpt2_onnx_path)
-        #self.ort_session = onnxruntime.InferenceSession(gpt2_onnx_path) 
-        # self で InferenceSession を持つと mlflow.pyfunc.save_model の pickle でこける
-        # https://github.com/microsoft/onnxruntime/issues/643
-        # https://github.com/microsoft/onnxruntime/pull/800
+        self.ort_session = onnxruntime.InferenceSession(gpt2_onnx_path)
         self.model_name_or_path = "rinna/japanese-gpt2-medium"
         self.tokenizer = self.get_tokenizer()
         self.num_layer = 24
@@ -24,24 +27,17 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
         else:    
             self.device =  torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def get_onnx_bytes(self, path):
-        with open(path, 'rb') as f:
-            return f.read()
-        
     def get_tokenizer(self):
-        import os
-        from transformers import T5Tokenizer
         if not os.path.exists(self.cache_dir):
             os.makedirs(self.cache_dir)
         tokenizer = T5Tokenizer.from_pretrained(self.model_name_or_path, cache_dir=self.cache_dir)
         tokenizer.padding_side = "left"
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.do_lower_case = True
-        #okenizer.add_special_tokens({'pad_token': '[PAD]'})
+        #tokenizer.add_special_tokens({'pad_token': '[PAD]'})
         return tokenizer
 
     def get_example_inputs(self, prompt_text):
-        import torch 
         encodings_dict = self.tokenizer.batch_encode_plus(prompt_text, padding=True)
 
         input_ids = torch.tensor(encodings_dict['input_ids'], dtype=torch.int64)
@@ -60,7 +56,6 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
         return input_ids, attention_mask, position_ids, empty_past
 
     def inference_with_io_binding(self, session, config, input_ids, position_ids, attention_mask, past, beam_select_idx, input_log_probs, input_unfinished_sents, prev_step_results, prev_step_scores, step, context_len):
-        from onnxruntime.transformers.gpt2_beamsearch_helper import Gpt2BeamSearchHelper, GPT2LMHeadModel_BeamSearchStep
         output_shapes = Gpt2BeamSearchHelper.get_output_shapes(batch_size=1,
                                                                context_len=context_len,
                                                                past_sequence_length=past[0].size(3),
@@ -82,9 +77,6 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
         """
         Update the inputs for next inference.
         """
-        import numpy
-        import torch
-
         last_state = (torch.from_numpy(output[0]).to(device)
                             if isinstance(output[0], numpy.ndarray) else output[0].clone().detach().cpu())
 
@@ -150,24 +142,10 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
     
         return inputs, ort_inputs, past
 
-    def extract_input_text(self, input_text):
-        import numpy
-        if type(input_text) is str:
-            return [input_text]
-        elif type(input_text) is dict:
-            return [input_text['input']]
-        elif type(input_text) is list:
-            return input_text
-        elif type(input_text) is numpy.ndarray:
-            return list(input_text)
-
     def predict(self, input_text):
-        import numpy
-        import torch
-        import onnxruntime
-        print("Text generation using", "OnnxRuntime with IO binding" if self.use_onnxruntime_io else "OnnxRuntime", "...")
-        
-        input_ids, attention_mask, position_ids, past = self.get_example_inputs(self.extract_input_text(input_text))
+
+        print("Text generation using", "OnnxRuntime with IO binding" if self.use_onnxruntime_io else "OnnxRuntime", "...")    
+        input_ids, attention_mask, position_ids, past = self.get_example_inputs([input_text])
         beam_select_idx = torch.zeros([1, input_ids.shape[0]]).long()
         input_log_probs = torch.zeros([input_ids.shape[0], 1])
         input_unfinished_sents = torch.ones([input_ids.shape[0], 1], dtype=torch.bool)
@@ -197,16 +175,12 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
         batch_size = input_ids.size(0)
         beam_size = self.beam_size
         context_length = input_ids.size(-1)
-
-        ort_session = onnxruntime.InferenceSession(self.onnx_bytes)
-        # self で保持すると mlflow.pyfunc.save_model の cloudpickle でこけるため、都度セッションを作成
-        # self.ort_session を使用する場合は下記 ort_session -> self.ort_session に書き換え
-
+        
         for step in range(self.num_tokens_to_produce):
             if self.use_onnxruntime_io:
-                outputs = self.inference_with_io_binding(ort_session, config, inputs['input_ids'], inputs['position_ids'], inputs['attention_mask'], past, inputs['beam_select_idx'], inputs['input_log_probs'], inputs['input_unfinished_sents'], inputs['prev_step_results'], inputs['prev_step_scores'], step, context_length)
+                outputs = self.inference_with_io_binding(self.ort_session, config, inputs['input_ids'], inputs['position_ids'], inputs['attention_mask'], past, inputs['beam_select_idx'], inputs['input_log_probs'], inputs['input_unfinished_sents'], inputs['prev_step_results'], inputs['prev_step_scores'], step, context_length)
             else:
-                outputs = ort_session.run(None, ort_inputs) 
+                outputs = self.ort_session.run(None, ort_inputs) 
             inputs, ort_inputs, past = self.update(outputs, step, batch_size, beam_size, context_length, inputs['attention_mask'], self.device)
 
             if not inputs['input_unfinished_sents'].any():
@@ -214,4 +188,37 @@ class ONNXrinnaGPT2(mlflow.pyfunc.PythonModel):
 
         predict_sentences = [self.tokenizer.decode(candidate, skip_special_tokens=True) for candidate in inputs['prev_step_results']]
         
-        return numpy.array(predict_sentences)
+        return predict_sentences
+
+
+def init():
+    global onnx_gpt2
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    # For multiple models, it points to the folder containing all deployed models (./azureml-models)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'rinna_gpt2_beam_step_search_optimized_gpt2_int8.onnx')
+    onnx_gpt2 = ONNXrinnaGPT2(model_path)
+    #onnx_gpt2.pre_load_ort() 
+
+def preprocess(input_data_json):
+    input_text = json.loads(input_data_json)['data']
+    return input_text
+    
+def run(input_data):
+    try:
+        # load in our data, convert to readable format
+        data = preprocess(input_data)
+        
+        # start timer
+        start = time.time()
+        result = onnx_gpt2.predict(data)
+        print(result)
+        #end timer
+        end = time.time()
+    
+        result_dict = {"results": result,
+                      "time_in_sec": end - start}
+    except Exception as e:
+        result_dict = {"error": str(e)}
+    
+    return result_dict
