b96b0e8c51e479bef9ed075716b7068dede3fd29
ShuntaIto <shunta.ito@microsoft.com>
2021-08-23 00:54:30 +0000

    Add executable py files

---
+++ examples/rinna-gpt2-predict/src/convert_model.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='test-model')
+    parser.add_argument('--version', type=int, default=37)
+    parser.add_argument('--save', default='rinna-GPT2-quantized-model')
+    args = parser.parse_args()
+
+    # キャッシュ保存用のディレクトリを用意
+    import os
+    cache_dir = os.path.join(".", "cache_models")
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    # Azure ML Workspace への接続
+    from azureml.core import Experiment, Workspace, Environment
+    from azureml.core.compute import ComputeTarget
+    from azureml.core import ScriptRunConfig
+    from azureml.core.runconfig import PyTorchConfiguration
+    from azureml.core.authentication import InteractiveLoginAuthentication
+    import mlflow
+
+    #interactive_auth = InteractiveLoginAuthentication(force=True,tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47")
+    ws = Workspace.from_config()
+
+    # AML 上で実行する場合は上記2行コメントアウト、以下実行
+    # ws = Workspace.from_config()
+
+    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
+
+    # ## モデル読み込み
+
+    client = mlflow.tracking.MlflowClient()
+
+    registered_model = client.get_model_version(name=args.model, version=args.version)
+    client.download_artifacts(registered_model.run_id, 'outputs/models', cache_dir)
+
+    # GPT-2 モデルにビームサーチを組み合わせるヘルパー class で読み込んだ GPT-2 モデルをラップ
+    from onnxruntime.transformers.gpt2_beamsearch_helper import Gpt2BeamSearchHelper, GPT2LMHeadModel_BeamSearchStep
+    from transformers import AutoConfig
+    import torch
+
+    model_name_or_path = os.path.join(cache_dir, 'outputs/models')
+    config = AutoConfig.from_pretrained(model_name_or_path, cache_dir=model_name_or_path)
+    model = GPT2LMHeadModel_BeamSearchStep.from_pretrained(model_name_or_path, config=config, batch_size=1, beam_size=4, cache_dir=cache_dir)
+    device = torch.device("cpu")
+    model.eval().to(device)
+
+
+    # 推論で使う関数用にモデルの情報を取得
+    num_attention_heads = model.config.n_head
+    hidden_size = model.config.n_embd
+    num_layer = model.config.n_layer
+
+
+    # tokenizer を読み込み
+    from transformers import T5Tokenizer
+    tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium", cache_dir=cache_dir)
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.do_lower_case = True
+
+
+    # ## PyTorch GPT-2 モデルをビームサーチの1ステップを含む ONNX に変換 
+
+
+    # ONNX に変換
+    onnx_model_path = os.path.join(cache_dir, "rinna_gpt2_beam_step_search.onnx")
+
+    if not os.path.exists(onnx_model_path):
+        Gpt2BeamSearchHelper.export_onnx(model, device, onnx_model_path) # add parameter use_external_data_format=True when model size > 2 GB
+    else:
+        print("GPT-2 ONNX model exists.")
+
+
+    # 最適化と量子化
+    from onnxruntime.transformers.gpt2_helper import Gpt2Helper, MyGPT2LMHeadModel
+    from onnxruntime.transformers.quantize_helper import QuantizeHelper
+
+    optimized_model_path = os.path.join(cache_dir, "rinna_gpt2_beam_step_search_optimized.onnx")
+    quantized_model_path = os.path.join(cache_dir, "rinna_gpt2_beam_step_search_optimized_int8.onnx")
+
+    if not os.path.exists(optimized_model_path):
+        Gpt2Helper.optimize_onnx(onnx_model_path, optimized_model_path, False, model.config.num_attention_heads, model.config.hidden_size)
+    else:
+        print("Optimized GPT-2 ONNX model exists.")
+
+    if not os.path.exists(quantized_model_path):   
+        QuantizeHelper.quantize_onnx_model(optimized_model_path, quantized_model_path)
+    else:
+        print("Quantized GPT-2 Int8 ONNX model exists.")
+
+    # 量子化した ONNX をモデルとして登録
+    mlflow.set_experiment('register_onnx')
+    with mlflow.start_run() as run:
+        remote_model_path = os.path.join('outputs','onnx', "rinna_gpt2_beam_step_search_optimized_int8.onnx")
+        mlflow.log_artifact(quantized_model_path, remote_model_path)
+        model_uri = "runs:/{}/".format(run.info.run_id) + remote_model_path
+        mlflow.register_model(model_uri, args.save)
---
+++ examples/rinna-gpt2-predict/src/deploy_onnx.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='test-model')
+    parser.add_argument('--cluster', default='aml-cluster')
+    parser.add_argument('--name', default='rinna-gpt2-aks')
+    args = parser.parse_args()
+
+
+    # # ONNX に変換したモデルを Kubernetes 上にデプロイ
+    import os
+    from azureml.core import Workspace
+    from azureml.core.conda_dependencies import CondaDependencies 
+    from azureml.core.model import InferenceConfig
+    from azureml.core.environment import Environment
+    from azureml.core.webservice import AksWebservice
+    from azureml.core.compute.aks import AksCompute 
+    from azureml.core.model import Model
+    import random
+    import numpy as np
+    import torch
+    import json
+    import requests
+
+    ws = Workspace.from_config()
+    ws
+
+    # 推論環境の定義ファイル生成と環境設定
+
+    env_file_path = os.path.join("src", "environment.yml")
+    score_file_path = os.path.join("src", "score.py")
+
+    env = Environment.from_conda_specification(name="rinna-predict-env", file_path=env_file_path)
+    env.register(ws)
+    inference_config = InferenceConfig(entry_script=score_file_path, environment=env)
+
+    # デプロイ設定
+    deploy_config = AksWebservice.deploy_configuration(
+        cpu_cores = 1,
+        memory_gb = 4,
+        tags = {'framework': 'onnx'},
+        auth_enabled = False,
+        description = 'rinna gpt-2'
+    )
+
+    target_aks = AksCompute(ws, args.cluster)
+
+    # モデル指定
+    model = Model(ws, args.model)
+
+    # デプロイ
+    print('Deploy ONNX model as API on AKS')
+    service_name = args.name
+    print("Service", service_name)
+    webservice = Model.deploy(ws, service_name, [model], inference_config, deploy_config, target_aks)
+    webservice.wait_for_deployment(True)
+    print(webservice.state)
+
+    # 推論
+    endpoint = webservice.scoring_uri
+    input_data = json.dumps({'data': "機械学習"})
+    res = requests.post(url=endpoint, data=input_data, headers={'Content-Type': 'application/json'})
+    print('Test API prediction')
+    res.json()
